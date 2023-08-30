e25d58247fd561c8a9bffcacb934e82f581298c2
Huy Ngo <huyngo878@yahoo.com>
2023-08-30 14:20:07 -0500

    Added P10055

---
+++ Assignments/P10055/main.cpp
@@ -0,0 +1,15 @@
+// Huy ngo
+// 4883 Programmming Techniques
+// August 28, 2023
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long x, y;
+
+  while (cin >> x >> y) {
+    cout << abs(x - y) << endl;
+  }
+}
